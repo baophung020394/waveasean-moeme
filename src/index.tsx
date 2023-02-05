208492888e299b75d81b9028a27e02a88b57b7cf
Phung Gia Bao <bao@aveapp.com>
2023-02-05 20:07:46 +0700

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import "@babel/polyfill";
 import App from "./App";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import 'bootstrap/docs/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById("root"));
